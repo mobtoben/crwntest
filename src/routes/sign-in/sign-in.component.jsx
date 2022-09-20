06f1d4a12f2ce978848e456d2ba3f1880d022953
mobtoben <rajakkertie@gmail.com>
2022-09-20 22:02:53 +0700

    testy heehe

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -13,6 +13,7 @@ const SignIn = () => {
     <div>
       <h1>Sign In Page</h1>
       <button onClick={logGoogleUser}>Sign in with Google</button>
+      <div>heheheahahaha</div>
     </div>
   );
 };
